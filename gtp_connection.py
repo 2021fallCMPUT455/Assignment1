438df6a7b1214f5e873bcb30a7aecdfd87e769e1
Bryce Nguyen <46576768+ubryce@users.noreply.github.com>
2021-09-20 15:25:34 -0600

    Change genmove play_move to gomoku_play_move

--- gtp_connection.py
+++ gtp_connection.py
@@ -355,7 +355,7 @@ class GtpConnection:
             move_coord = point_to_coord(move, self.board.size)
             move_as_string = format_point(move_coord)
             if self.board.is_legal(move, color):
-                self.board.play_move(move, color)
+                self.board.gomoku_play_move(move, color)
                 self.respond(move_as_string)
             else:
                 self.respond("Illegal move: {}".format(move_as_string))
